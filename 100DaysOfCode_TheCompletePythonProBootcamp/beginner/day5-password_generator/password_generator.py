f015cea9ee402bd945d5b6e636f79f3fe9ebf320
Izmyname <VI-MCMXCV@proton.me>
2025-04-14 00:51:02 +0500

    day 3: password generator - add exception handling

--- 100DaysOfCode_TheCompletePythonProBootcamp/beginner/day5-password_generator/password_generator.py
+++ 100DaysOfCode_TheCompletePythonProBootcamp/beginner/day5-password_generator/password_generator.py
@@ -4,21 +4,29 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
 password = []
 
-for l in range(0, nr_letters):
-    password.append(random.choice(letters))
+print("Welcome to the PyPassword Generator!")
 
-for s in range(0, nr_symbols):
-    password.append(random.choice(symbols))
+try:
+    nr_letters = int(input("How many letters would you like in your password?\n"))
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    
+except ValueError:
+    print("Use integers, please!")
+    
+else:
 
-for n in range(0, nr_numbers):
-    password.append(random.choice(numbers))
+
+    for l in range(0, nr_letters):
+        password.append(random.choice(letters))
+
+    for s in range(0, nr_symbols):
+        password.append(random.choice(symbols))
+
+    for n in range(0, nr_numbers):
+        password.append(random.choice(numbers))
 
 random.shuffle(password)
 
