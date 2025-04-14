eb1bc5a8d577a474645f11f1dc8933df07652c95
Izmyname <VI-MCMXCV@proton.me>
2025-04-14 14:06:38 +0500

    Day 2: Add exception handling for tip calculator

--- 100DaysOfCode_TheCompletePythonProBootcamp/beginner/day2-tip_calculator/tip_calculator.py
+++ 100DaysOfCode_TheCompletePythonProBootcamp/beginner/day2-tip_calculator/tip_calculator.py
@@ -1,12 +1,19 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
 
-tip_percent = tip / 100
-tip_result = bill * tip_percent
-total_bill = bill + tip_result
-amount_per_person = total_bill / people
-total = round(amount_per_person, 2)
+try:
+    bill = float(input("What was the total bill? $"))
+    tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+    people = int(input("How many people to split the bill? "))
 
-print(f"Each of you has to play ${total:.2f}")
+except ValueError:
+    print("use float for bill and integers for tip and people")
+    
+else:
+    
+    tip_percent = tip / 100
+    tip_result = bill * tip_percent
+    total_bill = bill + tip_result
+    amount_per_person = total_bill / people
+    total = round(amount_per_person, 2)
+
+    print(f"Each of you has to play ${total:.2f}")
