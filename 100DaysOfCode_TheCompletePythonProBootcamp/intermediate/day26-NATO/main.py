455f820af6606118849ee18b44a789ef206f9bfd
Izmyname <VI-MCMXCV@proton.me>
2025-04-27 17:08:20 +0500

    Udemy python bootcamp - add day 30

--- 100DaysOfCode_TheCompletePythonProBootcamp/intermediate/day26-NATO/main.py
+++ 100DaysOfCode_TheCompletePythonProBootcamp/intermediate/day26-NATO/main.py
@@ -1,11 +1,15 @@
 import pandas
 
 nato_csv = pandas.read_csv("./nato_phonetic_alphabet.csv")
-# nato_letters = {}
-
-# for (index, row) in nato_csv.iterrows():
-#     nato_letters[row.letter] = row.code
 nato_letters = {row.letter:row.code for (index, row) in nato_csv.iterrows()}
-name = input(" Enter your name: ").upper()
-nato_name = [nato_letters[l] for l in name]
+
+while True:
+    try:
+        name = input(" Enter your name: ").upper()
+        nato_name = [nato_letters[l] for l in name]
+    except KeyError:
+        print("Use English letters only, please")
+    else:
+        break
+    
 print(nato_name)
--- 100DaysOfCode_TheCompletePythonProBootcamp/intermediate/day29-MyPass/main.py
+++ 100DaysOfCode_TheCompletePythonProBootcamp/intermediate/day29-MyPass/main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -38,6 +40,12 @@ def save_password():
     website = website_entry.get()
     email = login_entry.get()
     password = pass_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
     
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showerror(title="Empty field", message="There shouldn't be empty fields")
@@ -45,12 +53,36 @@ def save_password():
         is_ok =  messagebox.askokcancel(title="nope", message=f"Website: {website}\nEmail: {email}\nPassword: {password}\n\n Proceed?")
         
         if is_ok:
-            with open("my_pass_database.txt", "a") as database:
-                database.write(f"{website} | {email} | {password}\n")
-        
-            website_entry.delete(0, 'end')
-            pass_entry.delete(0, 'end')
+            try:
+                with open("my_pass_database.json", "r") as database:
+                    # json.dump(new_data, database, indent=4)
+                    data = json.load(database)
+            except FileNotFoundError:
+                with open("my_pass_database.json", "w") as database:
+                    json.dump(new_data, database, indent=4)
+            else:  
+                with open("my_pass_database.json", "w") as database:
+                    data.update(new_data)
+                    json.dump(data, database, indent=4)
+            finally:
+                website_entry.delete(0, 'end')
+                pass_entry.delete(0, 'end')
 
+# ---------------------------------- SEARCH ------------------------------------- #
+def search():
+
+    website = website_entry.get()
+
+    try:
+        with open("my_pass_database.json", "r") as database:
+            data = json.load(database)
+    except FileNotFoundError:
+        return messagebox.showinfo(title="no data", message="The database is empty")
+    else:
+        if website in data:
+            return messagebox.showinfo(title="info", message=f"{website}\n\nEmail: {data[website]["email"]}\nPassword: {data[website]["password"]}")   
+        else:
+            return messagebox.showinfo(title="website not found", message=f"No saved data for {website}")
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Password Manager")
@@ -64,8 +96,8 @@ canvas.grid(column=1, row=0)
 website_label = tk.Label(text="Website: ")
 website_label.grid(column=0, row=1)
 
-website_entry = tk.Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2,sticky='we')
+website_entry = tk.Entry(width=21)
+website_entry.grid(column=1, row=1,sticky='we')
 website_entry.focus()
 
 login_label = tk.Label(text="Email/Username: ")
@@ -86,5 +118,8 @@ gen_pass_button.grid(column=2, row=3)
 
 add_button = tk.Button(text="Add", width=36, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2, sticky='we')
+
+search_button = tk.Button(text="Search", width=14, command=search)
+search_button.grid(column=2, row=1, sticky='we')
 
 window.mainloop()
